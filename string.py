b28baf0faa3be1784b948a23847c6e882681c375
subhani-mabu <pattansubhani4@gmail.com>
2025-05-22 17:17:36 +0530

    Update string.py

--- string.py
+++ string.py
@@ -1,6 +1,7 @@
-string_1 = "pattan mabu Subhani"
+string_1 = "yamini gurram"
 unique_chars = ""
 for each_char in string_1:
   if each_char not in unique_chars:
     unique_chars += each_char
 print (unique_chars) 
+
